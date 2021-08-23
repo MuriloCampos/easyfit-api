2d370a2d3e8f3dc76bd6ec946a4b321d8cbe77fe
Murilo Campos <murilo13campos@gmail.com>
2021-08-23 11:40:46 -0400

    defining sport entity

--- src/sports/entities/sport.entity.ts
+++ src/sports/entities/sport.entity.ts
@@ -1,1 +1,21 @@
-export class Sport {}
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Sport {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  updated_at: Date;
+}
