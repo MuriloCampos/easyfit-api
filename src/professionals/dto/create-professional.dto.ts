1fc4b1612dcc60cc5394d234746481de293a0500
Murilo Campos <murilo13campos@gmail.com>
2021-09-02 11:30:15 -0400

    improving swagger docs on the endpoints where students and professionals are created

--- src/professionals/dto/create-professional.dto.ts
+++ src/professionals/dto/create-professional.dto.ts
@@ -1,8 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Length, IsIn, IsNumber } from 'class-validator';
+
 export class CreateProfessionalDto {
+  @IsNumber()
+  @ApiProperty()
   age: number;
+
+  @IsIn(['male', 'female', 'not informed'])
+  @ApiProperty({ enum: ['male', 'female', 'not informed'] })
   gender: 'male' | 'female' | 'not informed';
+
+  @ApiProperty()
   expertise: string[];
+
+  @ApiProperty()
+  @Length(5, 255)
   name: string;
+
+  @Length(5, 500)
+  @ApiProperty()
   bio: string;
+
+  @ApiProperty()
+  @Length(5, 255)
   email: string;
 }
--- src/students/dto/create-student.dto.ts
+++ src/students/dto/create-student.dto.ts
@@ -1,25 +1,35 @@
 import { Length, IsIn, IsNumber } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateStudentDto {
   @IsNumber()
+  @ApiProperty()
   age: number;
 
   @IsNumber()
+  @ApiProperty()
   weight: number;
 
   @IsNumber()
+  @ApiProperty()
   height: number;
 
   @IsIn(['male', 'female', 'not informed'])
+  @ApiProperty({ enum: ['male', 'female', 'not informed'] })
   gender: 'male' | 'female' | 'not informed';
 
   @Length(5, 500)
+  @ApiProperty()
   goals: string;
 
+  @ApiProperty()
   sports: string[];
 
+  @ApiProperty()
   @Length(5, 255)
   name: string;
 
+  @ApiProperty()
+  @Length(5, 255)
   email: string;
 }
