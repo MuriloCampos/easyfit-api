2f17e6de3507b41a9e778edcfcc38a91ed82968c
Murilo Campos <mupereira@microsoft.com>
2021-12-06 09:06:52 -0400

    adding class entity to prod typeorm config

--- src/config/orm.config prod.ts
+++ src/config/orm.config prod.ts
@@ -4,12 +4,13 @@ import { Professional } from 'src/professionals/entities/professional.entity';
 import { Sport } from 'src/sports/entities/sport.entity';
 import { Student } from 'src/students/entities/student.entity';
 import { User } from 'src/users/entities/user.entity';
+import { Class } from 'src/classes/entities/class.entity';
 
 export default registerAs(
   'orm.config.prod',
   (): TypeOrmModuleOptions => ({
     type: 'postgres',
-    entities: [Professional, Sport, Student, User],
+    entities: [Professional, Sport, Student, User, Class],
     synchronize: true,
     url: process.env.DATABASE_URL,
     extra: {
